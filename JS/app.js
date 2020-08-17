21a70018e1c8e88f4a25b911ac0218d1e83c9ef3
Sharon Walters <SHARONMARIE919@GMAIL.COM>
2020-08-17 11:37:19 -0400

    DONE

--- JS/app.js
+++ JS/app.js
@@ -1,24 +1,24 @@
-import { Person, render } from "./personalProfile.js"
+import { Person, render } from "../JS/personalProfile.js";
 
 let people = [
-    new Person("Bob", "bob-ross.jpg"),
-    new Person("Frida", "frida-kahlo.jpg"),
-    new Person("Dali", "dali.jpg")
+  new Person("Bob", "../img/bob-ross.jpg"),
+  new Person("Frida", "../img/frida-kahlo.jpg"),
+  new Person("Dali", "../img/dali.jpg"),
 ];
 
 window.onload = () => {
-    renderList(people);
+  renderList(people);
 };
 
 function renderList(people) {
-    let html = "";
-    html += `<ul>`;
-    for (let person of people) {
-        html += "<li>";
-        html += render(person);
-        html += "</li>";
-    }
-    html += "</ul>";
+  let html = "";
+  html += `<ul>`;
+  for (let person of people) {
+    html += "<li>";
+    html += render(person);
+    html += "</li>";
+  }
+  html += "</ul>";
 
-    document.getElementById("list").innerHTML = html;
+  document.getElementById("list").innerHTML = html;
 }
